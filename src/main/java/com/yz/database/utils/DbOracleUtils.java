17880ec82934e3e5cc5cb0d6da5821be3753ce21
CeresY <asuka.y@qq.com>
2018-02-28 11:06:26 +0800

    数据库工具类

---
+++ src/main/java/com/yz/database/utils/DbOracleUtils.java
@@ -0,0 +1,95 @@
+package com.yz.database.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * mysql工具类
+ * @author yangzhan-xps13
+ *
+ */
+public class DbOracleUtils {
+	static final String DRIVER = "oracle.jdbc.OracleDriver";
+	static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+	static final String USER = "vbi";
+	static final String PWD = "123456";
+	static Connection conn = null;
+	static Statement statement = null;
+	static PreparedStatement ps = null;
+	
+	public static Statement getStatement() {
+		conn = getConnection();
+		try {
+			statement = conn.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// statement.executeQuery(sql)
+		return statement;
+	}
+	
+	public static PreparedStatement getPs(String sql) {
+		try {
+			if(conn == null) {
+				conn = getConnection();
+			}
+			
+			if(ps == null) {
+				ps = conn.prepareStatement(sql);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ps;
+	}
+	
+	public static Connection getConnection() {
+		if(conn != null) {
+			return conn;
+		}
+		try{
+            Class.forName(DRIVER);
+            System.out.println("成功加载Oracle驱动！");
+            conn = DriverManager.getConnection(URL, USER, PWD);
+        }catch(ClassNotFoundException e1){
+            System.out.println("找不到Oracle驱动!");
+            e1.printStackTrace();
+        } catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void close() {
+		if(statement != null) {
+			try {
+				statement.close();
+				System.out.println("关闭statement");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(ps != null) {
+			try {
+				ps.close();
+				System.out.println("关闭ps");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				conn.close();
+				System.out.println("关闭conn");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/com/yz/database/utils/DbUtils.java
@@ -0,0 +1,106 @@
+package com.yz.database.utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbUtils {
+	
+	public static enum DB {
+		ORACLE, MYSQL;
+	}
+	
+	/**
+	 * 获取处理对象
+	 * @param type
+	 * @return
+	 */
+	public static Statement getStatement(DB type) {
+		if(type.equals(DB.ORACLE)) {
+			return DbOracleUtils.getStatement();
+		} else if(type.equals(DB.MYSQL)) {
+			return DbMysqlUtils.getStatement();
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * 获取预处理对象
+	 * @param type
+	 * @return
+	 */
+	public static PreparedStatement getPs(String sql, DB type) {
+		if(type.equals(DB.ORACLE)) {
+			return DbOracleUtils.getPs(sql);
+		} else if(type.equals(DB.MYSQL)) {
+			return DbMysqlUtils.getPs(sql);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * 获取数据库连接
+	 * @param type
+	 * @return
+	 */
+	public static Connection getConnection(DB type) {
+		if(type.equals(DB.ORACLE)) {
+			return DbOracleUtils.getConnection();
+		} else if(type.equals(DB.MYSQL)) {
+			return DbMysqlUtils.getConnection();
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * 关闭数据库连接
+	 * @param type
+	 */
+	public static void close(DB type) {
+		if(type.equals(DB.ORACLE)) {
+			DbOracleUtils.close();
+		} else if(type.equals(DB.MYSQL)) {
+			DbMysqlUtils.close();
+		}
+	}
+	
+	/**
+	 * 执行查询结果
+	 * @param sql
+	 * @param type
+	 * @return
+	 */
+	public static ResultSet executeQuery(String sql, DB type) {
+		PreparedStatement pstmt = DbUtils.getPs(sql, type);
+		ResultSet resultSet = null;
+		try {
+			resultSet = pstmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultSet;
+	}
+	
+	/**
+	 * 执行dml语句
+	 * @param sql
+	 * @param type
+	 * @return
+	 */
+	public static int executeUpdate(String sql, DB type) {
+		PreparedStatement pstmt = DbUtils.getPs(sql, type);
+		int i= -1;
+		try {
+			i = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
+}
