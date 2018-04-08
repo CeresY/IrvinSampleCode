4766f9943fda444a6129bf53bfe91cb4979eb845
CeresY <asuka.y@qq.com>
2018-04-08 10:01:31 +0800

    jedis工具类

--- src/main/java/web/redis/JedisTest.java
+++ src/main/java/web/redis/JedisTest.java
@@ -1,10 +1,19 @@
 package web.redis;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -16,7 +25,8 @@ import redis.clients.jedis.JedisPoolConfig;
 public class JedisTest {
 	
 	private static JedisPool pool;
-	private static Jedis jedis = null;
+	private static Jedis jedis = null; //单机版
+    // private JedisCluster jedis; //redis-cluster集群版打开这个,注销上面那个
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -30,6 +40,30 @@ public class JedisTest {
 		pool.destroy();
 	}
 	
+	// 有关集群的处理
+	/**
+    public void setup() {
+        //连接redis服务器，192.168.17.185:6379  单机版链接方式
+        //jedis = new Jedis("192.168.17.185", 6379);
+
+        //这是redis-cluster 集群版,稍微有点不同,如果是集群,使用打开下面代码,注销上面的代码
+        //创建一个JedisCluster对象
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("192.168.17.185", 7001));
+        nodes.add(new HostAndPort("192.168.17.185", 7002));
+        nodes.add(new HostAndPort("192.168.17.185", 7003));
+        nodes.add(new HostAndPort("192.168.17.185", 7004));
+        nodes.add(new HostAndPort("192.168.17.185", 7005));
+        nodes.add(new HostAndPort("192.168.17.185", 7006));
+        //在nodes中指定每个节点的地址
+        //jedisCluster在系统中是单例的。
+        jedis= new JedisCluster(nodes);
+
+        //权限认证
+        //jedis.auth("admin");
+    }
+    */
+	
 	@Test
 	public void set1() {
 		String reVal = jedis.set("xx", "abc");
@@ -37,4 +71,118 @@ public class JedisTest {
 		String reVal2 = jedis.get("xx");
 		System.out.println(reVal2);
 	}
+
+    /**
+     * redis存储字符串
+     */
+    @Test
+    public void testString() {
+        //-----添加数据----------
+        jedis.set("name","xinxin");//向key-->name中放入了value-->xinxin
+        System.out.println(jedis.get("name"));//执行结果：xinxin
+
+        jedis.append("name", " is my lover"); //拼接
+        System.out.println(jedis.get("name"));
+
+        jedis.del("name");  //删除某个键
+        System.out.println(jedis.get("name"));
+        //设置多个键值对  redis-cluster不支持多键操作,如果使用集群版的注销掉下面这句话
+        //jedis.mset("name","liuling","age","23","qq","476777XXX");
+        jedis.incr("age"); //进行加1操作
+        System.out.println(jedis.get("name") + "-" + jedis.get("age") + "-" + jedis.get("qq"));
+
+    }
+
+    /**
+     * redis操作Map
+     */
+    @Test
+    public void testMap() {
+        //-----添加数据----------
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "xinxin");
+        map.put("age", "22");
+        map.put("qq", "123456");
+        jedis.hmset("user",map);
+        //取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
+        //第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
+        List<String> rsmap = jedis.hmget("user", "name", "age", "qq");
+        System.out.println(rsmap);
+
+        //删除map中的某个键值
+        jedis.hdel("user","age");
+        System.out.println(jedis.hmget("user", "age")); //因为删除了，所以返回的是null
+        System.out.println(jedis.hlen("user")); //返回key为user的键中存放的值的个数2
+        System.out.println(jedis.exists("user"));//是否存在key为user的记录 返回true
+        System.out.println(jedis.hkeys("user"));//返回map对象中的所有key
+        System.out.println(jedis.hvals("user"));//返回map对象中的所有value
+
+        Iterator<String> iter=jedis.hkeys("user").iterator();
+        while (iter.hasNext()){
+            String key = iter.next();
+            System.out.println(key+":"+jedis.hmget("user",key));
+        }
+    }
+
+    /**
+     * jedis操作List
+     */
+    @Test
+    public void testList(){
+        //开始前，先移除所有的内容
+        jedis.del("java framework");
+        System.out.println(jedis.lrange("java framework",0,-1));
+        //先向key java framework中存放三条数据
+        jedis.lpush("java framework","spring");
+        jedis.lpush("java framework","struts");
+        jedis.lpush("java framework","hibernate");
+        //再取出所有数据jedis.lrange是按范围取出，
+        // 第一个是key，第二个是起始位置，第三个是结束位置，jedis.llen获取长度 -1表示取得所有
+        System.out.println(jedis.lrange("java framework",0,-1));
+
+        jedis.del("java framework");
+        jedis.rpush("java framework","spring");
+        jedis.rpush("java framework","struts");
+        jedis.rpush("java framework","hibernate");
+        System.out.println(jedis.lrange("java framework",0,-1));
+    }
+
+    /**
+     * jedis操作Set
+     */
+    @Test
+    public void testSet(){
+        //添加
+        jedis.sadd("user","liuling");
+        jedis.sadd("user","xinxin");
+        jedis.sadd("user","ling");
+        jedis.sadd("user","zhangxinxin");
+        jedis.sadd("user","who");
+        //移除noname
+        jedis.srem("user","who");
+        System.out.println(jedis.smembers("user"));//获取所有加入的value
+        System.out.println(jedis.sismember("user", "who"));//判断 who 是否是user集合的元素
+        System.out.println(jedis.srandmember("user"));
+        System.out.println(jedis.scard("user"));//返回集合的元素个数
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        //jedis 排序
+        //注意，此处的rpush和lpush是List的操作。是一个双向链表（但从表现来看的）
+        jedis.del("a");//先清除数据，再加入数据进行测试
+        jedis.rpush("a", "1");
+        jedis.lpush("a","6");
+        jedis.lpush("a","3");
+        jedis.lpush("a","9");
+        System.out.println(jedis.lrange("a",0,-1));// [9, 3, 6, 1]
+        System.out.println(jedis.sort("a")); //[1, 3, 6, 9]  //输入排序后结果
+        System.out.println(jedis.lrange("a",0,-1));
+    }
+
+    /*@Test
+    public void testRedisPool() {
+        RedisUtil.getJedis().set("newname", "中文测试");
+        System.out.println(RedisUtil.getJedis().get("newname"));
+    }*/
 }
