3c6d97131860fd7e08c74cc071021bd09211363e
CeresY <asuka.y@qq.com>
2019-06-27 21:08:58 +0800

    thread实现定时任务

---
+++ src/main/java/thread/timer/Woker.java
@@ -0,0 +1,51 @@
+package thread.timer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description //
+ * @Author yz
+ * @Date 2019-6-27
+ * @Vesion 1.0
+ **/
+public class Woker extends Thread{
+    BlockingQueue<String> q;
+
+    Woker(BlockingQueue<String> q) {
+        this.q = q;
+    }
+
+    /*public void run() {
+        long start = System.currentTimeMillis();
+        long end = start + 2; //just wait for two milliseconds
+
+        while (true) {
+            String x = q.poll();
+            if(x != null) {
+                System.out.println(x + " from " + this.getName());
+            } else if(System.currentTimeMillis() >= end){
+                System.out.println("No new message since two milliseconds, killing thread " + this.getName());
+                interrupt();
+            }
+
+            if(interrupted()) {
+                break;
+            }
+        }
+    }*/
+
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                String x = q.poll(2, TimeUnit.SECONDS);
+                if (x == null)
+                    System.out.println("No new message since two seconds, killing thread " + this.getName());
+                Thread.currentThread().interrupt();
+                System.out.println(x + "\tfrom\t" + this.getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
