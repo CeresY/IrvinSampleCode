48a29cea52ab3d164e4186952198a4afbc38ea71
CeresY <asuka.y@qq.com>
2019-07-11 14:34:47 +0800

    jdbc工具类

---
+++ src/main/java/grammar/JdbcExtractBaseServiceImpl.java
@@ -0,0 +1,11 @@
+package grammar;
+
+/**
+ * @Description //
+ * @Author yz
+ * @Date 2019-7-11
+ * @Vesion 1.0
+ **/
+public abstract class JdbcExtractBaseServiceImpl implements IJdbcExtractService{
+    public abstract void setMetaData();
+}
