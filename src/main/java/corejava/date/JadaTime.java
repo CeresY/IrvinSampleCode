338cc7cd91f75011a9157359e7382e07a7969bf9
CeresY <asuka.y@qq.com>
2019-08-23 01:45:00 +0800

    jada工具类; yml工具类

--- src/main/java/corejava/date/JadaTime.java
+++ src/main/java/corejava/date/JadaTime.java
@@ -1,18 +1,72 @@
 package corejava.date;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
- * @Description //
+ * @Description // https://blog.csdn.net/psh18513234633/article/details/79408096
  * @Author yz
  * @Date 2019-7-17
  * @Vesion 1.0
  **/
 public class JadaTime {
+    static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
     @Test
-    public void test1() {
-        System.out.println(LocalDate.now().toString() + " " + LocalTime.now().toString("HH:mm:ss"));
+    public void testDateTime() {
+        System.out.println(LocalDateTime.now());
+
+        DateTime dt = new DateTime();
+        System.out.println("dt-property-1: " + dt.monthOfYear().getAsText());
+        System.out.println("dt-property-2: " + dt.monthOfYear().withMaximumValue());
+        System.out.println("dt-property-3: " + dt.monthOfYear().roundCeilingCopy());
+        System.out.println(dt.toString());
+
+        String now3 = DateTime.now().toString(formatter);
+        System.out.println("now3: "+now3);
+
+        Date d1 = DateTime.now().toDate();
+        System.out.println("d1: "+d1);
+
+        System.out.println("dt2: " + DateTime.now().withDayOfMonth(3).toString(formatter));
+        System.out.println("dt3: " + DateTime.now().withHourOfDay(4).toString(formatter));
+        System.out.println("dt4: " + DateTime.now().withTimeAtStartOfDay().toString(formatter));
+
+        // 下面的代码将计算五年前的第二个月的最后一天
+        DateTime now = DateTime.now();
+        DateTime then = now.minusYears(5) // five years ago
+                .monthOfYear()     // get monthOfYear property
+                .setCopy(2)        // set it to February
+                .dayOfMonth()      // get dayOfMonth property
+                .withMaximumValue();// the last day of the month
+        System.out.println("五年前的第二个月的最后一天: " + then.toString(formatter));
+        System.out.println("五年前的第二个月的最后一天: " + then.toString("yyyy-MM-dd HH:mm:ss"));
+
+        System.out.println("");
+        DateTime dateTime = DateTime.now();
+        System.out.println("Format1: " + dateTime.toString(ISODateTimeFormat.basicDateTime()));
+        System.out.println("Format2: " + dateTime.toString(ISODateTimeFormat.basicDateTimeNoMillis()));
+        System.out.println("Format3: " + dateTime.toString(ISODateTimeFormat.basicOrdinalDateTime()));
+        System.out.println("Format4: " + dateTime.toString(ISODateTimeFormat.basicWeekDateTime()));
     }
+
+    @Test
+    public void testLocalDate() {
+        System.out.println(LocalDate.now().toString() + " " + LocalTime.now().toString("HH:mm:ss"));
+
+        LocalDate start = new LocalDate();
+        LocalDate end = new LocalDate().minusDays(1);
+        System.out.println("day1: " + Days.daysBetween(start, end).getDays());
+    }
+
+
 }
---
+++ src/main/java/util/YmlUtils.java
@@ -0,0 +1,127 @@
+package util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * yml文件属性读取类
+ */
+@Slf4j
+public class YmlUtils {
+
+    private static String default_file = "application.yml";
+
+    private static Map<String, String> result = new HashMap<>();
+
+    private static Map<String, String> resultDefalut = new HashMap<>();
+
+    private static Map<String, String> resultActive = new HashMap<>();
+
+    public static void main(String[] args) {
+        /*for(int i=0; i<3; i++) {
+            Map<String, String> map = getYmlActive();
+            for(Map.Entry<String,String> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            System.out.println("====================="+i+"==========================");
+        }*/
+        for (int i = 0; i < 3; i++) {
+            System.out.println("==============="+i+"=================");
+            System.out.println(getValueDefalut("spring.jackson.time-zone"));
+            System.out.println(getValueActive("spring.datasource.url"));
+        }
+    }
+
+    public static String getValueActive(String key) {
+        return getYmlActive().get(key);
+    }
+
+    /**
+     * 根据yml中的key获取值
+     * @param key
+     * @return
+     */
+    public static String getValueDefalut(String key) {
+        return getYmlDefalut().get(key);
+    }
+
+    public static Map<String, String> getYmlDefalut() {
+        if(MapUtils.isEmpty(resultDefalut)) {
+            resultDefalut = getYmlByFileName(null);
+        }
+        return resultDefalut;
+    }
+
+    public static Map<String, String> getYmlActive() {
+        String file = "application-"+getValueDefalut("spring.profiles.active")+".yml";
+        if(MapUtils.isEmpty(resultActive)) {
+            resultActive = getYmlByFileName(file);
+        }
+        return resultActive;
+    }
+
+    /**
+     * 根据文件名获取yml的文件内容
+     * @return
+     */
+    public static Map<String, String> getYmlByFileName(String file) {
+        log.info("解析文件{}",file);
+        result = new HashMap<>();
+        if (file == null)
+            file = default_file;
+        try {
+            Resource resource = new ClassPathResource(file);
+            //通过stream去读取，打包后才能读到。
+            InputStream in = resource.getInputStream();
+            Yaml props = new Yaml();
+            Object obj = props.loadAs(in, Map.class);
+            Map<String, Object> param = (Map<String, Object>) obj;
+            for (Map.Entry<String, Object> entry : param.entrySet()) {
+                String key = entry.getKey();
+                Object val = entry.getValue();
+                if (val instanceof Map) {
+                    forEachYaml(key, (Map<String, Object>) val);
+                } else {
+                    result.put(key, val.toString());
+                }
+            }
+        } catch (Exception ex) {
+
+        }
+        return result;
+    }
+
+    /**
+     * 遍历yml文件，获取map集合
+     * @param key_str
+     * @param obj
+     * @return
+     */
+    private static Map<String, String> forEachYaml(String key_str, Map<String, Object> obj) {
+        for (Map.Entry<String, Object> entry : obj.entrySet()) {
+            String key = entry.getKey();
+            Object val = entry.getValue();
+
+            String str_new = "";
+            if (StringUtils.isNotEmpty(key_str)) {
+                str_new = key_str + "." + key;
+            } else {
+                str_new = key;
+            }
+            if (val instanceof Map) {
+                forEachYaml(str_new, (Map<String, Object>) val);
+            } else {
+                result.put(str_new, val.toString());
+            }
+        }
+        return result;
+    }
+}
